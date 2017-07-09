69c0e9a82df82a96add71376802c35dcc8d2db49
Arup Sengupta <arupsenmail@gmail.com>
2017-07-09 15:22:56 +0530

    response header changed

--- app.js
+++ app.js
@@ -2,6 +2,11 @@ var express = require('express');
 var app = express();
 var db = require('./db');
 
+app.use(function(req, res, next){
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  return next();
+});
+
 var UserController = require('./User/UserController');
 app.use('/users', UserController);
 
