a0dfb4848dcb6baf4dc86e6d9c57e3123844c476
Arup Sengupta <arupsenmail@gmail.com>
2017-07-16 18:35:14 +0530

    socket.io added

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
-var express = require('express');
-var app = express();
+var app = require('express')();
+var server = require('http').Server(app);
 var db = require('./db');
+var io = require('socket.io')(server);
+
+server.listen(3000);
+
+io.on('connection',function(socket){
+  console.log('hello');
+  socket.emit('news', {hello : 'world'});
+  socket.on('other event',function(data){
+    console.log(data);
+  });
+});
 
 app.use(function(req, res, next){
   res.setHeader("Access-Control-Allow-Origin", "*");
