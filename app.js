cca5c4435b11008056df1d77d7b17105d3f99c11
Arup Sengupta <arupsenmail@gmail.com>
2017-07-16 20:18:40 +0530

    socket.io fixed on server.js

--- app.js
+++ app.js
@@ -1,15 +1,5 @@
 var app = require('express')();
-var server = require('http').Server(app);
 var db = require('./db');
-var io = require('socket.io')(server);
-
-io.on('connection',function(socket){
-  console.log('hello');
-  socket.emit('news', {hello : 'world'});
-  socket.on('other event',function(data){
-    console.log(data);
-  });
-});
 
 app.use(function(req, res, next){
   res.setHeader("Access-Control-Allow-Origin", "*");
--- server.js
+++ server.js
@@ -4,3 +4,13 @@ var port = process.env.PORT || 8080;
 var server = app.listen(port, function(){
   console.log('Express server listening on port ' + port);
 });
+
+var io = require('socket.io')(server);
+
+io.on('connection',function(socket){
+  console.log('hello');
+  socket.emit('news', {hello : 'world'});
+  socket.on('other event',function(data){
+    console.log(data);
+  });
+});
