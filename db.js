836ba3998962e334cc75e2a5311c5cdfe2c18e58
Arup Sengupta <arupsenmail@gmail.com>
2017-07-24 23:23:24 +0530

    location updated

--- db.js
+++ db.js
@@ -1,3 +1,3 @@
 var mongoose = require('mongoose');
-// mongoose.connect('mongodb://root:root@ds151702.mlab.com:51702/heroku_xkvg7hsj');
-mongoose.connect('mongodb://root:root@ds149122.mlab.com:49122/epark');
+mongoose.connect('mongodb://root:root@ds151702.mlab.com:51702/heroku_xkvg7hsj');
+//mongoose.connect('mongodb://root:root@ds149122.mlab.com:49122/epark');
