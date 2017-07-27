dcb18f57346aff49b8975b3b36afc46536abe5e8
arupsengupta <arupsenmail@gmail.com>
2017-07-27 18:39:58 +0530

    index.html added

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 var app = require('express')();
 var db = require('./db');
+var path = require('path');
+var express = require('express');
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(function(req, res, next){
   res.setHeader("Access-Control-Allow-Origin", "*");
@@ -10,6 +14,9 @@ var UserController = require('./User/UserController');
 var LocationController = require('./Location/LocationController');
 var SensorController = require('./Sensor/SensorController');
 var BookingController = require('./Booking/BookingController');
+app.get('/',function(req,res){
+	res.sendFile(path.join(__dirname,'public','index.html'));
+});
 app.use('/users', UserController);
 app.use('/location',LocationController);
 app.use('/pushData', SensorController);
