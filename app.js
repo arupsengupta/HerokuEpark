da02fc3d52e5e9cfa6aaa4ddeaae65418df1e2ae
arupsengupta <arupsenmail@gmail.com>
2017-07-29 17:13:47 +0530

    allow delete method

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(function(req, res, next){
   res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods","GET,POST,PUT,DELETE");
   return next();
 });
 
