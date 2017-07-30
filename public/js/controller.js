18ab35c29f7b2ef8880d55d1cb3c4df4ebe5140f
Arup Sengupta <arup@localhost.localdomain>
2017-07-30 16:44:59 +0530

    location add fixed

--- public/js/controller.js
+++ public/js/controller.js
@@ -4,7 +4,7 @@ app.controller('LoginController', function($scope, $location){
 		userName : "",
 		password : ""
 	};
-	
+
 	$scope.loginAdmin = function(){
 		if(($scope.admin.userName != null && $scope.admin.userName != "") && ($scope.admin.password != null && $scope.admin.password != "") && ($scope.admin.userName == "admin" && $scope.admin.password == "admin12#")){
 			$location.path("home");
@@ -17,7 +17,7 @@ app.controller('LoginController', function($scope, $location){
 
 app.controller('UserController', function($scope, $http, $window){
 	$scope.userArray = [];
-	
+
 	$http({
 		  method: 'GET',
 		  url: 'https://arupepark.herokuapp.com/users',
@@ -28,8 +28,8 @@ app.controller('UserController', function($scope, $http, $window){
 			}
 		}, function errorCallback(response){
 			 alert(JSON.stringify(response));
-		});	
-	
+		});
+
 	$scope.deleteUser = function(userID){
 		$http({
 			  method: 'DELETE',
@@ -38,17 +38,17 @@ app.controller('UserController', function($scope, $http, $window){
 			alert("User account deleted succcessfully");
 			$window.location.reload();
 		}, function errorCallback(response){
-			alert("Error in deleting this user."); 
-		});	
+			alert("Error in deleting this user.");
+		});
 	};
 });
 
 
 app.controller('AddLocationController', function($scope, $http, $window){
 	$scope.startingTime = ["00:00", "01:00", "02:00", "03:00", "04:00", "05:00", "06:00", "07:00", "08:00", "09:00", "10:00", "11:00", "12:00", "13:00", "14:00", "15:00", "16:00", "17:00", "18:00", "19:00", "20:00", "21:00", "22:00", "23:00"];
-	
+
 	$scope.closingTime = ["00:00", "01:00", "02:00", "03:00", "04:00", "05:00", "06:00", "07:00", "08:00", "09:00", "10:00", "11:00", "12:00", "13:00", "14:00", "15:00", "16:00", "17:00", "18:00", "19:00", "20:00", "21:00", "22:00", "23:00"];
-	
+
 	$scope.newLocation = {
 		name : '',
 		address : '',
@@ -59,22 +59,22 @@ app.controller('AddLocationController', function($scope, $http, $window){
 		startingTime : 0,
 		closingTime : 0
 	};
-	
+
 	$scope.resultSet = [];
-	
+
 	$http({
 		  method: 'GET',
 		  url: 'https://arupepark.herokuapp.com/location',
 		  headers: { 'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'}
 		}).then(function successCallback(response){
-			for(var i=0;i<response.data.length;i++){  
+			for(var i=0;i<response.data.length;i++){
 			    $scope.resultSet.push(response.data[i]);
 			}
 		}, function errorCallback(response){
 			 alert(JSON.stringify(response));
 	});
-	
-	
+
+
 	$scope.deleteLocation = function(locationID){
 		$http({
 			  method: 'DELETE',
@@ -83,10 +83,10 @@ app.controller('AddLocationController', function($scope, $http, $window){
 			alert("Location successfully deleted");
 			$window.location.reload();
 		}, function errorCallback(response){
-			alert("Error in deleting this Location."); 
-		});	
+			alert("Error in deleting this Location.");
+		});
 	};
-	
+
 	$scope.createLocation = function(){
 		$http({
 			  method: 'POST',
@@ -98,21 +98,19 @@ app.controller('AddLocationController', function($scope, $http, $window){
 							  "lng": $scope.newLocation.longitude,
 							  "address": $scope.newLocation.address,
 							  "hourly_price": $scope.newLocation.rate,
-							  "number_of_slot": $scope.newLocation.numberOfSlots,
+							  "parking_slot": $scope.newLocation.numberOfSlots,
 							  "booked_slot": 0,
-							  "opening_hours":{
-							      "start": $scope.newLocation.startingTime,
-							      "end": $scope.newLocation.closingTime
-							  }
+							  "opening_hours_start": $scope.newLocation.startingTime,
+							  "opening_hours_end": $scope.newLocation.closingTime
 				       })
 			}).then(function successCallback(response){
 				alert("Location successfully created");
 				$window.location.reload();
 			}, function errorCallback(response){
-				alert("Oops! This Location could not be created"); 
-			});	
+				alert("Oops! This Location could not be created");
+			});
 	};
-	
+
 });
 
 
@@ -122,7 +120,7 @@ app.controller('AdminController', function($scope, $http){
 	$scope.totalSensorCount = 0;
 	$scope.healthySensorCount = 0;
 	$scope.faultySensorCount = 0;
-	
+
 	var d = new Date();
 	$scope.currentDate = d.getDate() + "-" + (d.getMonth() + 1) + "-" + d.getFullYear();
 	$scope.hours = d.getHours();
@@ -136,9 +134,9 @@ app.controller('AdminController', function($scope, $http){
 		$scope.currentHour = $scope.hours;
 		$scope.meridian = "AM";
 	}
-	
-	$scope.currentTime = $scope.currentHour + ":" + d.getMinutes() + " " + $scope.meridian ; 
-	
+
+	$scope.currentTime = $scope.currentHour + ":" + d.getMinutes() + " " + $scope.meridian ;
+
 	$http({
 		  method: 'GET',
 		  url: 'https://arupepark.herokuapp.com/location',
@@ -148,7 +146,7 @@ app.controller('AdminController', function($scope, $http){
 			 $scope.totalSensorCount = response.data[0].number_of_slot;
 			 for(var i=0;i<response.data.length;i++){
 				 if((i+1) < response.data.length){
-					 $scope.totalSensorCount = $scope.totalSensorCount + response.data[i+1].number_of_slot;	 
+					 $scope.totalSensorCount = $scope.totalSensorCount + response.data[i+1].number_of_slot;
 				 }else{
 					 $scope.totalSensorCount = $scope.totalSensorCount;
 				 }
@@ -157,7 +155,7 @@ app.controller('AdminController', function($scope, $http){
 				 $scope.closingHoursTest = response.data[i].opening_hours.end;
 				 $scope.openingHours = $scope.checkTimeFormatNew($scope.openingHoursTest);
 				 $scope.closingHours = $scope.checkTimeFormatNew($scope.closingHoursTest);
-				 
+
 				 for(var j=0; j<response.data[i].parking_arr.length; j++){
 					 $scope.parkingSlot.push(response.data[i].parking_arr[j]);
 					 if(response.data[i].parking_arr[j].status == 'available'){
@@ -172,25 +170,25 @@ app.controller('AdminController', function($scope, $http){
 		 }, function errorCallback(response){
 			 alert(JSON.stringify(response));
 		});
-	
-	
+
+
 	$scope.getSlotDetails = function(locationID, slotID){
 		$scope.bookingPresentFlag = false;
-		
+
 		$scope.slotIDDisplay = slotID;
 		$scope.bookingStartTime = 0;
 		$scope.bookingSpan = 0;
 		$scope.startTime = 0;
 		$scope.bookingStatus = "";
 		$scope.userOTP = 0;
-		$scope.userOTPStatus =""; 
+		$scope.userOTPStatus ="";
 		$scope.bookingSpan = 0;
-		
+
 		$scope.bookedUserName = "";
 	    $scope.bookedVehicleNumber = "";
 	    $scope.bookedUserEmail = "";
-	    $scope.bookedUserMob = ""; 
-		
+	    $scope.bookedUserMob = "";
+
 		$http({
 			  method: 'GET',
 			  url: 'https://arupepark.herokuapp.com/booking',
@@ -202,23 +200,23 @@ app.controller('AdminController', function($scope, $http){
 						$scope.getUserDetails(response1.data[i].user_id);
 						$scope.startTime = response1.data[i].start_time;
 						$scope.bookingSpan = response1.data[i].hours;
-						
+
 						if($scope.startTime > 0 && $scope.bookingSpan > 0){
 							$scope.bookingPresentFlag = true;
 						}else{
 							$scope.bookingPresentFlag = false;
 						}
-						
+
 						$scope.bookingStatus = response1.data[i].status;
 						$scope.userOTP = response1.data[i].otp.value;
 						$scope.otpStatus = response1.data[i].otp.matched;
-						
+
 						if($scope.otpStatus == false){
 							$scope.userOTPStatus = "No, awaiting car's arrival";
 						}else{
 							$scope.userOTPStatus = "Yes, done";
 						}
-						
+
 						$scope.checkTimeFormat($scope.startTime);
 						break;
 					}
@@ -227,8 +225,8 @@ app.controller('AdminController', function($scope, $http){
 				 alert(JSON.stringify(response1));
 			});
 	};
-	
-	
+
+
 	$scope.checkTimeFormat = function(testTime){
 		if(testTime > 12){
 			$scope.bookingStartTime = (testTime - 12) + ":" + "00 PM";
@@ -239,7 +237,7 @@ app.controller('AdminController', function($scope, $http){
 			$scope.bookingStartTime = testTime + ":" + "00 AM";
 		}
 	};
-	
+
 	$scope.checkTimeFormatNew = function(testTime){
 		if(testTime > 12){
 			return (testTime - 12) + ":" + "00 PM";
@@ -250,12 +248,12 @@ app.controller('AdminController', function($scope, $http){
 			return testTime + ":" + "00 AM";
 		}
 	};
-	
+
 	$scope.getUserDetails = function(userID){
 		$scope.bookedUserName = "";
 	    $scope.bookedVehicleNumber = "";
 	    $scope.bookedUserEmail = "";
-	    $scope.bookedUserMob = ""; 
+	    $scope.bookedUserMob = "";
 		$http({
 			  method: 'GET',
 			  url: 'https://arupepark.herokuapp.com/users',
@@ -274,5 +272,5 @@ app.controller('AdminController', function($scope, $http){
 				 alert(JSON.stringify(response2));
 			});
 	};
-	
+
 });
