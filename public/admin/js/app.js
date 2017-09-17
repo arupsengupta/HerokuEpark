19295bee41a210e0fbafbae20106371beb26fe03
Soumyadeep Ghosh <soumyadeep.sam@gmail.com>
2017-09-17 12:56:35 +0530

    New Website and Separate Admin

---
+++ public/admin/js/app.js
@@ -0,0 +1,22 @@
+var app = angular.module('adminApp', ['ngRoute']);
+
+app.config(function($routeProvider, $locationProvider){
+	$locationProvider.hashPrefix('');                        //TO CHANGE THE !# to # in ANGULAR ROUTING
+    $routeProvider
+	.when('/', {
+		templateUrl : 'templates/login.html',
+		controller : 'LoginController'
+	})
+	.when('/home', {
+		templateUrl : 'templates/home.html',
+		controller : 'AdminController'
+	})
+	.when('/user', {
+		templateUrl : 'templates/userDetails.html',
+		controller : 'UserController'
+	})
+    .when('/addLocation', {
+		templateUrl : 'templates/addLocation.html',
+		controller : 'AddLocationController'
+	});
+});
--- public/js/app.js
+++ public/js/app.js
@@ -1,22 +1,70 @@
-var app = angular.module('adminApp', ['ngRoute']);
+var app = angular.module('eParkWeb', ['ngMaterial', 'directive.g+signin', 'ngMessages']);
 
-app.config(function($routeProvider, $locationProvider){
-	$locationProvider.hashPrefix('');                        //TO CHANGE THE !# to # in ANGULAR ROUTING
-    $routeProvider
-	.when('/', {
-		templateUrl : 'templates/login.html',
-		controller : 'LoginController'
-	})
-	.when('/home', {
-		templateUrl : 'templates/home.html',
-		controller : 'AdminController'
-	})
-	.when('/user', {
-		templateUrl : 'templates/userDetails.html',
-		controller : 'UserController'
-	})
-    .when('/addLocation', {
-		templateUrl : 'templates/addLocation.html',
-		controller : 'AddLocationController'
+app.controller('HomeController', function($scope, $mdMenu){
+	var originatorEv;
+
+    $scope.openMenu = function($mdMenu, ev) {
+      originatorEv = ev;
+      $mdMenu.open(ev);
+    };
+});
+
+app.controller('HistoryController', function($scope){
+	
+});
+
+app.controller('BookingController', function($scope){
+	$scope.mapPath = 'images/map.png';
+	$scope.imagePath = 'images/ePark.png';
+	
+	$scope.rate = 30;
+	$scope.availabilityStatus = "Slots Available";
+	
+	this.myDate = new Date();
+	this.isOpen = false;
+	
+	$scope.data = {
+		 group1 : 'Two-Wheeler',
+		 group2 : 'Four-Wheeler',
+    };
+});
+
+app.controller('ProfileController', function($scope){
+	$scope.user = {
+		name : '',
+		firstName : '',
+		imageUrl : '',
+		email : '',
+		mobile : '',
+		vehicleReg : '',
+		vehicleType : ''
+	};
+	
+	$scope.user.mobile = "9874332133";
+	$scope.user.vehicleReg = "WB 08D 1145";
+	$scope.text = "";
+	
+	var qrcode = new QRCode(document.getElementById("qrcode"), {
+		width : 100,
+		height : 100
 	});
+
+	$scope.$on('event:google-plus-signin-success', function (event, authResult) {
+    	$scope.user.name = authResult.w3.ig;
+    	$scope.user.firstName = authResult.w3.ofa;
+    	$scope.user.email = authResult.w3.U3; 
+    	
+    	if(authResult.w3.Paa != null || authResult.w3.Paa != ""){
+    		$scope.user.imageUrl = authResult.w3.Paa;
+    	}else{
+    		$scope.user.imageUrl = "images/user.png";
+    	}
+    	
+    	qrcode.makeCode(authResult.w3.ig);
+        $scope.$apply();
+    });
+
+    $scope.$on('event:google-plus-signin-failure', function (event, authResult) {
+      console.log('Not signed into Google Plus.');
+    });
 });
