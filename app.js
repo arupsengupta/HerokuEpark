53dd85a5b7b75e15b4faf8ce33095b6582aac529
arupsengupta <arupsenmail@gmail.com>
2017-11-07 03:40:01 +0530

    changes

--- app.js
+++ app.js
@@ -12,8 +12,13 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(function(req, res, next){
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods","GET,POST,PUT,DELETE,OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
   return next();
 });
+
+// app.OPTIONS("*", function(req, res){
+//   res.send(200);
+// });
 
 var UserController = require('./User/UserController');
 var LocationController = require('./Location/LocationController');
