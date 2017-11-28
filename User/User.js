d564c5bf02205f9bd21d9954d58a38d1a95c43d2
arupsengupta <arupsenmail@gmail.com>
2017-11-29 01:35:07 +0530

    commit

--- User/User.js
+++ User/User.js
@@ -4,6 +4,7 @@ var UserSchema = new mongoose.Schema({
   email: String,
   phone: String,
   vehicle_no: String,
+  vehicle_type: {type: Number, default: 4},
   password: String,
   device_token: {type: String, default: ''}
 });
