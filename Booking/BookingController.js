6cf6d026872fd657b3d34b1f307fb5b5e41918e3
arupsengupta <arupsenmail@gmail.com>
2017-11-29 01:49:43 +0530

    commit

--- Booking/BookingController.js
+++ Booking/BookingController.js
@@ -17,7 +17,7 @@ router.post('/',function(req, res){
   var loc_start_time = date.format(now, 'hh:mm A');
   var current_date = date.format(now, 'DD-MM-YYYY');
 
-  Booking.findOne({date: current_date, parking_id: req.body.parking_id, operator_id: req.body.operator_id, active: true, 'manualData.reg_number': req.body.reg_number, vehicle_type: req.body.wheels}).populate('parking_id').exec(function(err, booking){
+  Booking.findOne({date: current_date, parking_id: req.body.parking_id, active: true, 'manualData.reg_number': req.body.reg_number, vehicle_type: req.body.wheels}).populate('parking_id').exec(function(err, booking){
     if(err) return res.status(503).send(err);
     // console.log(booking);
     if(booking){
@@ -98,29 +98,87 @@ router.post('/',function(req, res){
 // book using QR scan
 router.post('/qr/:id',function(req, res){
   var now = new Date();
-  var loc_start_time = date.format(now, 'HH:mm');
+  var loc_start_time = date.format(now, 'hh:mm A');
+  var current_date = date.format(now, 'DD-MM-YYYY');
+  Booking.findOne({date: current_date, parking_id: req.body.parking_id, active: true, user_id: req.params.id}).populate('parking_id').exec(function(err, booking){
+    if(err) return res.status(500).send('Error getting booking details');
+    if(booking){
+      var start_time =  date.parse(booking.start_time, 'hh:mm A');
+      var end_time =  date.parse(loc_start_time, 'hh:mm A');
+      var duration = date.subtract(end_time, start_time).toMinutes();
+      var hours = parseInt(duration / 60) + 1;
+      var cost = 0;
+      if(booking.vehicle_type === 2){
+        cost = booking.parking_id.fare.two * hours;
+      }else{
+        cost = booking.parking_id.fare.four * hours;
+      }
 
-  User.findById(req.params.id, function(err, user){
-    if(err) return res.status(500).send('Error getting user details');
-    if(user){
-      Booking.create({
-        user_id: user._id,
-        parking_id: req.body.parking_id,
-        operator_id: req.body.operator_id,
-        start_time: loc_start_time,
-        type: 'app'
-      },function(err, booking){
-        if(err) return res.status(500).send("Cannot book");
-        //req.app.io.emit('pending',{parking_id: booking.parking_id, slot_id : booking.slot_id, start_time: booking.start_time, hours: booking.hours});
-        res.status(200).send(booking);
-        //res.status(200).send(booking);
-      });
+      Booking.findByIdAndUpdate(booking._id, {status: 'completed', end_time: loc_start_time, mins: duration, active: false, fare: cost}, {new: true}, function(err, result){
+    		if(err) return res.status(500).send("Error occured while booking");
+    		// console.log('Booking has been completed');
+        Location.findById(req.body.parking_id,'number_of_slot',function(err, location){
+          if(err) return res.status(500).send(err);
+            var bookedCarCount = 0;
+            var bookedBikeCount = 0;
+      	  Booking.count({date: current_date, parking_id: location._id ,status: 'Booked', active: true, vehicle_type: 4},function(err, c){
+      	  	bookedCarCount = c;
+            Booking.count({date: current_date, parking_id: location._id ,status: 'Booked', active: true, vehicle_type: 2},function(err, c){
+        	  	bookedBikeCount = c;
+              var availCount = {
+                two : location.number_of_slot.two - bookedBikeCount,
+                four : location.number_of_slot.four - bookedCarCount
+              };
+              req.app.io.emit('count-changed',{parking_id: location._id, value: availCount});
+        	  });
+      	  });
+        });
+    		res.status(200).send(result);
+    	});
     }else{
-      res.status(403).send('User not found');
+      User.findById(req.params.id, function(err, user){
+        if(err) return res.status(500).send('Error getting user details');
+        if(user){
+          Booking.create({
+            user_id: user._id,
+            parking_id: req.body.parking_id,
+            operator_id: req.body.operator_id,
+            start_time: loc_start_time,
+            end_time: '',
+            mins: 0,
+            date: current_date,
+            type: 'app',
+            vehicle_type: user.vehicle_type,
+            manualData: {
+              reg_number: user.vehicle_no,
+            },
+            timestamp: Date.now()
+          },function(err, booking){
+            if(err) return res.status(500).send("Cannot book");
+            Location.findById(req.body.parking_id,'number_of_slot',function(err, location){
+              if(err) return res.status(500).send(err);
+                var bookedCarCount = 0;
+                var bookedBikeCount = 0;
+          	  Booking.count({date: current_date, parking_id: location._id ,status: 'Booked', active: true, vehicle_type: 4},function(err, c){
+          	  	bookedCarCount = c;
+                Booking.count({date: current_date, parking_id: location._id ,status: 'Booked', active: true, vehicle_type: 2},function(err, c){
+            	  	bookedBikeCount = c;
+                  var availCount = {
+                    two : location.number_of_slot.two - bookedBikeCount,
+                    four : location.number_of_slot.four - bookedCarCount
+                  };
+                  req.app.io.emit('count-changed',{parking_id: location._id, value: availCount});
+            	  });
+          	  });
+            });
+            res.status(200).send(booking);
+          });
+        }else{
+          res.status(403).send('User not found');
+        }
+      });
     }
   });
-
-
 });
 
 // create a new booking by
