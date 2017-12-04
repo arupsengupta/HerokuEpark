12e25121771c349a19a2a7666008b111f7dfb631
Soumyadeep Ghosh <soumyadeep.sam@gmail.com>
2017-12-04 21:52:55 +0530

    New Changes in Monitoring Logs

--- public/monitorLogs/app.js
+++ public/monitorLogs/app.js
@@ -10,9 +10,17 @@ app.controller('LoggingController', function($scope, $http){
 	}).then(function successCallBack(response){
 		for(var i=0; i < response.data.length ; i++){
 			if(response.data[i].operator_id == '5a228a34c21aef0014009923'){
-				$scope.results.push(response.data[i]);
+				if(response.data[i].date == '02-12-2017' || response.data[i].date == '03-12-2017'){
+					//DO NOTHING
+				}else{
+					$scope.results.push(response.data[i]);	
+				}
 			}else if(response.data[i].operator_id == '5a22890bc21aef0014009922'){
-				$scope.results1.push(response.data[i]);
+				if(response.data[i].date == '02-12-2017' || response.data[i].date == '03-12-2017'){
+					//DO NOTHING
+				}else{
+					$scope.results1.push(response.data[i]);
+				}
 			}
 		}
 	}, function errorCallBack(response){
